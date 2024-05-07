fabc0523daa3ec1b4fe0f0283af84227954d68c6
Agustin <agustin.ledezma@ucb.edu.bo>
2024-05-07 11:22:15 -0400

    No forma parte del ciclo tdd se carga lo que estaba en la pizzara y se empieza la tarea

---
+++ src/buscar.spec.js
@@ -0,0 +1,33 @@
+
+function buscarProyecto(nombre, proyectos){
+    if (proyectos.length === 0){
+        return "";
+    }
+    for(const proyecto of proyectos){
+        if(proyecto === nombre){
+            return proyecto;
+        }
+    }
+}
+
+
+describe("buscarProyecto", () => {
+    it("No se encuentra proyectos cuando no se tiene ninguno en la lista de proyectos", () => {
+        let proyectos = [];
+        expect (buscarProyecto("ejercicio1", proyectos)).toEqual("");
+    });
+    it("Se ecuentra el proyecto cuando hay solo 1 proyecto", () => {
+        let proyectos = [];
+        proyectos.push("miUnicoProyecto");
+        expect (buscarProyecto("miUnicoProyecto", proyectos)).toEqual("miUnicoProyecto");
+    });
+    it("encuentra un proyecto cuando el mismo existe en una lista de varios proyectos", () => {
+        let proyectos = [];
+        proyectos.push("Proyecto1");
+        proyectos.push("Proyecto2");
+        proyectos.push("Proyecto3");
+        expect(buscarProyecto("Proyecto2", proyectos)).toEqual("Proyecto2");
+    });
+    
+
+});
